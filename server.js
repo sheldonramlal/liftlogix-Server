d69521c5c8ec5a4a161a210ede20121707de13b6
sheldonramlal <sheldon.ramlal@my.uwi.edugit config --global user.name sheldon>
2023-12-11 16:54:19 -0400

    updated server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 const cors = require('cors')
+const path = require('path')
 
 // express app
 const app = express()
@@ -13,6 +14,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
@@ -22,6 +24,11 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
+
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
