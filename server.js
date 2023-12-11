038c973f5bd808f82113d8374c73ce735cd275bd
sheldonramlal <sheldon.ramlal@my.uwi.edugit config --global user.name sheldon>
2023-12-11 17:02:46 -0400

    updated server again

--- server.js
+++ server.js
@@ -22,13 +22,14 @@ app.use((req, res, next) => {
 })
 
 // routes
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
 app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
 
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
